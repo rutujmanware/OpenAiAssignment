9af7ca8c6ee81b74eb21ae1f5652746f46a66259
Rituj Manware <manwarerutuj@gmail.com>
2023-10-24 18:33:30 +0530

    query generator and resonse generator added

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 import { getJson } from "serpapi";
-import {} from 'dotenv/config'
+import {} from "dotenv/config";
 // import { Configuration, OpenAIApi } from "openai";
-// //org-mRnIQGZmZIhbhPu70uxuzfBk
-// //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
-// const configuration = new Configuration({
-//   organization: "org-mRnIQGZmZIhbhPu70uxuzfBk",
-//   apiKey: "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",
-// });
 
-// const openai = new OpenAIApi(configuration);
-
-// openai
-//   .createChatCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages: [{ role: "user", content: "Hello" }],
-//   })
-//   .then((result) => {
-//     console.log(result);
-//   });
 import OpenAI from "openai";
 import readline from "readline";
-
+// import { query } from "express";
+let arr = [];
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
@@ -30,33 +15,96 @@ const userInterface = readline.createInterface({
   output: process.stdout,
 });
 
+var googleSearch = (input) => {
+  return new Promise((resolve, reject) => {
+    const ansapi = [];
+    getJson(
+      {
+        engine: "google",
+        q: input,
+        api_key: process.env.REACT_APP_SERP_API_KEY,
+      },
+      (json) => {
+        json["organic_results"].map((data) => {
+          ansapi.push(data.snippet);
+        });
+        resolve(ansapi);
+      }
+    );
+  });
+};
+
+
+var OpenAiResponse = async (query) => {
+  // Define arr as an array to store responses.
+
+  try {
+    const result = await openai.chat.completions.create({
+      messages: [{ role: "user", content: query }],
+      model: "gpt-3.5-turbo",
+    });
+
+    arr.push(result.choices[0].message.content);
+    return result.choices[0].message.content;
+
+    // Call userInterface.prompt() if it's defined.
+    // userInterface.prompt();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 userInterface.prompt();
 userInterface.on("line", async (input) => {
-  const ansapi = [];
-  await getJson({
-    engine: "google",
-    q: input,
-    api_key: process.env.REACT_APP_SERP_API_KEY
-  }, (json) => {
-    // console.log(json["organic_results"]);
-    json["organic_results"].map((data) => {
-      ansapi.push(data.snippet);
-    })
-  });
+  let googleData = [];
+  if (arr.length > 30) {
+    arr.shift();
+  }
+  arr.push(input);
+  const googleSearchPromt = `Your task is to generate a query to ask so that you could find relevant results \
+  -First check if chat history and user input can make sense togethor or not. \
+  -then if they make sense create a relevant query to search on serpapi \
+  chat history: ${arr}\
+  user Input: ${input}\
+  -strictly return only the query to be searched on google`;
+  try {
+    const res = await OpenAiResponse(googleSearchPromt);
+    // console.log(res,"GoogleSearch query\n");
+    const data = await googleSearch(res);
+    googleData = data;
 
 
 
-  const prompt = `Hey GPT if user is interacting with you please do interact with user as you do normally on the other side if user want some facts then only refer the data provided to you, this is the interaction of user ${input} please refer the data I have privided only if required ${ansapi}, please do remember the context of the chat what is going on with the user user may ask or inquire regarding previously asked question, user may also ask to provide calculation regarding numerical data in the previous answer please do provide calculated answer not only the formulae.`;
-   await openai.chat.completions
-    .create({
-      messages: [{ role: "user", content: prompt  }],
-      model: "gpt-3.5-turbo",
-    })
-    .then((result) => {
-      console.log(result.choices[0].message.content);
-      userInterface.prompt();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  const openAiPrompt = `Your task is to answer user with the most relevent answer you can generate.\
+  -Try to understand the input of the user which is direct interaction of the user.\
+  -If user is trying to interact with you reply with your understanding\
+  -Decide if the query asked is pure fact or mathematical calculations based on previous chat or history\
+  -check for the history if the question asked is context based or not with the previous record available\
+  -check if google search results contain answer to the query provided to you\
+  -check if you can answer the query based on your past knowledge\
+  -check if you can perform numerical operation and reach final answer then calculate and provide final answer.\
+  
+  input: ${input}\
+  history:${arr}\
+  google search results: ${googleData}`;
+  try{
+    const res = await OpenAiResponse(openAiPrompt);
+    if(arr.length>30){
+      arr.shift();
+    }
+    arr.push(res);
+    // console.log("History",arr,"\n")
+    // console.log(googleData,"data to be used finally");
+    console.log(res);
+    userInterface.prompt();
+    
+  }catch(err){
+    console.log(err,"Error");
+  }
+ 
+
 });
